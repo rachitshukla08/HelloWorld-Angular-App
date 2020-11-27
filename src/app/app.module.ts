15672ea17e833cf59293a2fbcebf866c36cda736
unknown <rachitshukla08@gmail.com>
2020-11-27 16:53:05 +0530

    Added: HelloWorld Angular app to display Hello from BridgeLabz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+//app.module.ts - Indicates this is a typescript file. Essentially provides strong type checking
+
+//importing core modules from angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,11 +11,15 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  //Various imports needed for application
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  //To provide services like http services, they are added to providers
   providers: [],
+  //The root component which is the main view of the application. Only the root module has this property and indicates the component
+  //which is going to be bootstrapped
   bootstrap: [AppComponent]
 })
 export class AppModule { }
