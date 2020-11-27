a1c083131955c7c90bcd31beb5d258736e76dd36
unknown <rachitshukla08@gmail.com>
2020-11-27 18:41:47 +0530

    Added: Regex to test if user has given proper username or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   imgUrl = "";
   url;
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit() : void{
     this.title = 'Hello from BridgeLabz.';
@@ -21,5 +22,15 @@ export class AppComponent {
   onClick($event){
     console.log("Save button is clicked!",$event);
     window.open(this.url,"_blank");
+  }
+
+  onInput($event){
+    console.log("Change event occured!",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$'); 
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect";
   }
 }
